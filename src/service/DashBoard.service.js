6c69038f30a29d61179f92f7aeb69a1779be1ebc
Chayapat <jaroonsoft@hotmail.com>
2024-11-13 19:27:28 +0700

    dashboard

--- src/service/DashBoard.service.js
+++ src/service/DashBoard.service.js
@@ -1,5 +1,6 @@
 import { requestService as api } from "./Request.service"  
 const API_URL = { 
+  API_MINSTOCK: `/dashboard/get-min-stock.php`,  
   API_SAMPLELIST: `/dashboard/get-sample-list.php`,  
   API_SAMPLEREQUEST_LIST: `/dashboard/get-sample-request-list.php`,  
   API_SAMPLEREQUEST_DETAIL: `/dashboard/get-sample-request-details.php`,  
@@ -8,7 +9,7 @@ const API_URL = {
 };
   
 const DashBoardService = () => { 
-  
+  const minstock = (parm = {}) => api.post(`${API_URL.API_MINSTOCK}`, parm, { ignoreLoading : true });
   const samplelist = (parm = {}, load = false) => api.post(`${API_URL.API_SAMPLELIST}`, parm, { ignoreLoading : load });
   const sample_requestlist = (parm = {}, load = false) => api.post(`${API_URL.API_SAMPLEREQUEST_LIST}`, parm, { ignoreLoading : load });
   const sample_requestdetail = (parm = {}, load = false) => api.post(`${API_URL.API_SAMPLEREQUEST_DETAIL}`, parm, { ignoreLoading : load });
@@ -16,6 +17,7 @@ const DashBoardService = () => {
   const statistics = (load = false) => api.post(`${API_URL.API_STATISTICS}`, null, { ignoreLoading : load });
 
   return {
+    minstock,
     samplelist,
     sample_requestlist,
     sample_requestdetail,
