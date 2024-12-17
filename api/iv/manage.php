0f1a18ab47f36427e93e6cdd9f83c431234bcb19
Chayapat <jaroonsoft@hotmail.com>
2024-12-17 16:38:56 +0700

    delete invoice

--- api/iv/manage.php
+++ api/iv/manage.php
@@ -1,40 +1,40 @@
-<?php 
-ob_start(); 
-include_once(dirname(__FILE__, 2)."/onload.php");
-include_once(dirname(__FILE__, 2)."/common/fnc-code.php");
+<?php
+ob_start();
+include_once(dirname(__FILE__, 2) . "/onload.php");
+include_once(dirname(__FILE__, 2) . "/common/fnc-code.php");
 
 $db = new DbConnect;
 $conn = $db->connect();
 $conn->beginTransaction();
 http_response_code(400);
 try {
-    $action_date = date("Y-m-d H:i:s"); 
+    $action_date = date("Y-m-d H:i:s");
     $action_user = $token->userid;
     // echo $action_user;
 
-    if ($_SERVER["REQUEST_METHOD"] == "POST"){
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $rest_json = file_get_contents("php://input");
-        $_POST = json_decode($rest_json, true); 
+        $_POST = json_decode($rest_json, true);
         extract($_POST, EXTR_OVERWRITE, "_");
 
         // var_dump($_POST);
         $sql = "insert iv_master (`ivcode`, `ivdate`, `cuscode`,total_price,total_pay,`remark`,created_by,updated_by) 
         values (:ivcode,:ivdate,:cuscode,:total_price,:total_pay,
         :remark,:action_user,:action_user)";
-        
-        $stmt = $conn->prepare($sql);
-        if(!$stmt) throw new PDOException("Insert data error => {$conn->errorInfo()}"); 
 
-        $header = (object)$header;  
+        $stmt = $conn->prepare($sql);
+        if (!$stmt) throw new PDOException("Insert data error => {$conn->errorInfo()}");
+
+        $header = (object)$header;
         $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);
         $stmt->bindParam(":ivdate", $header->ivdate, PDO::PARAM_STR);
-        $stmt->bindParam(":cuscode", $header->cuscode, PDO::PARAM_STR);  
-        $stmt->bindParam(":total_price", $header->total_price, PDO::PARAM_STR); 
-        $stmt->bindParam(":total_pay", $header->total_pay, PDO::PARAM_STR); 
-        $stmt->bindParam(":remark", $header->remark, PDO::PARAM_STR); 
-        $stmt->bindParam(":action_user", $action_user, PDO::PARAM_STR); 
+        $stmt->bindParam(":cuscode", $header->cuscode, PDO::PARAM_STR);
+        $stmt->bindParam(":total_price", $header->total_price, PDO::PARAM_STR);
+        $stmt->bindParam(":total_pay", $header->total_pay, PDO::PARAM_STR);
+        $stmt->bindParam(":remark", $header->remark, PDO::PARAM_STR);
+        $stmt->bindParam(":action_user", $action_user, PDO::PARAM_STR);
 
-        if(!$stmt->execute()) {
+        if (!$stmt->execute()) {
             $error = $conn->errorInfo();
             throw new PDOException("Insert data error => $error");
             die;
@@ -62,8 +62,8 @@ try {
 
         update_ivcode($conn);
 
-        $total_price=$header->total_price;
-        $total_pay=$header->total_pay;
+        $total_price = $header->total_price;
+        $total_pay = $header->total_pay;
 
         $sql = "insert into iv_detail (ivcode,recode)
         values (:ivcode,:recode)";
@@ -79,6 +79,26 @@ try {
                 $error = $conn->errorInfo();
                 throw new PDOException("Insert data error => $error");
             }
+
+            $sql = "
+            update receipt 
+            set
+            doc_status = 'ออกใบวางบิลแล้ว',
+            updated_date = CURRENT_TIMESTAMP(),
+            updated_by = :action_user
+            where recode = :recode";
+
+            $stmt1 = $conn->prepare($sql);
+            if (!$stmt1) throw new PDOException("Insert data error => {$conn->errorInfo()}");
+
+            $stmt1->bindParam(":action_user", $action_user, PDO::PARAM_INT);
+            $stmt1->bindParam(":recode", $val->recode, PDO::PARAM_STR);
+
+            if (!$stmt1->execute()) {
+                $error = $conn->errorInfo();
+                throw new PDOException("Insert data error => $error");
+                die;
+            }
         }
 
         $sql = "insert into iv_payment (ivcode,paydate,price,payment_type,bank,bank_name_th,bank_name,bank_no,remark)
@@ -87,7 +107,7 @@ try {
         if (!$stmt2) throw new PDOException("Insert data error => {$conn->errorInfo()}");
 
         // $detail = $detail;  
-        
+
 
         foreach ($payment as $ind => $pay) {
             $pay = (object)$pay;
@@ -106,7 +126,7 @@ try {
             }
         }
 
-        
+
 
         if ($total_price == $total_pay) {
             $sql = "
@@ -121,15 +141,14 @@ try {
             if (!$stmt) throw new PDOException("Insert data error => {$conn->errorInfo()}");
 
             $stmt->bindParam(":action_user", $action_user, PDO::PARAM_INT);
-            $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);          
+            $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);
 
             if (!$stmt->execute()) {
                 $error = $conn->errorInfo();
                 throw new PDOException("Insert data error => $error");
                 die;
             }
-        }
-        else if ($total_pay == 0) {
+        } else if ($total_pay == 0) {
             $sql = "
             update iv_master 
             set
@@ -142,15 +161,14 @@ try {
             if (!$stmt) throw new PDOException("Insert data error => {$conn->errorInfo()}");
 
             $stmt->bindParam(":action_user", $action_user, PDO::PARAM_INT);
-            $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);          
+            $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);
 
             if (!$stmt->execute()) {
                 $error = $conn->errorInfo();
                 throw new PDOException("Insert data error => $error");
                 die;
             }
-        }
-        else {
+        } else {
             $sql = "
             update iv_master 
             set
@@ -174,11 +192,10 @@ try {
 
         $conn->commit();
         http_response_code(200);
-        echo json_encode(array("data"=> array("code" => $code,"total_pay" => $total_pay,"total_price" => $total_price)));
-
-    } else  if($_SERVER["REQUEST_METHOD"] == "PUT"){
+        echo json_encode(array("data" => array("code" => $code, "total_pay" => $total_pay, "total_price" => $total_price)));
+    } else  if ($_SERVER["REQUEST_METHOD"] == "PUT") {
         $rest_json = file_get_contents("php://input");
-        $_PUT = json_decode($rest_json, true); 
+        $_PUT = json_decode($rest_json, true);
         extract($_PUT, EXTR_OVERWRITE, "_");
         // var_dump($_POST);
 
@@ -195,26 +212,26 @@ try {
         where ivcode = :ivcode";
 
         $stmt = $conn->prepare($sql);
-        if(!$stmt) throw new PDOException("Insert data error => {$conn->errorInfo()}"); 
+        if (!$stmt) throw new PDOException("Insert data error => {$conn->errorInfo()}");
 
-        $header = (object)$header; 
-        
+        $header = (object)$header;
+
         $stmt->bindParam(":ivdate", $header->ivdate, PDO::PARAM_STR);
-        $stmt->bindParam(":cuscode", $header->cuscode, PDO::PARAM_STR);   
-        $stmt->bindParam(":total_price", $header->total_price, PDO::PARAM_STR); 
-        $stmt->bindParam(":total_pay", $header->total_pay, PDO::PARAM_STR); 
-        $stmt->bindParam(":remark", $header->remark, PDO::PARAM_STR); 
-        $stmt->bindParam(":action_user", $action_user, PDO::PARAM_INT);  
-        $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR); 
+        $stmt->bindParam(":cuscode", $header->cuscode, PDO::PARAM_STR);
+        $stmt->bindParam(":total_price", $header->total_price, PDO::PARAM_STR);
+        $stmt->bindParam(":total_pay", $header->total_pay, PDO::PARAM_STR);
+        $stmt->bindParam(":remark", $header->remark, PDO::PARAM_STR);
+        $stmt->bindParam(":action_user", $action_user, PDO::PARAM_INT);
+        $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);
 
-        if(!$stmt->execute()) {
+        if (!$stmt->execute()) {
             $error = $conn->errorInfo();
             throw new PDOException("Insert data error => $error");
             die;
         }
 
-        $total_price=$header->total_price;
-        $total_pay=$header->total_pay;
+        $total_price = $header->total_price;
+        $total_pay = $header->total_pay;
 
         $sql = "delete from iv_detail where ivcode = :ivcode";
         $stmt3 = $conn->prepare($sql);
@@ -238,7 +255,25 @@ try {
                 throw new PDOException("Insert data error => $error");
             }
 
-            $total_price+=($val->price*$val->qty)*(1-($val->discount/100));
+            $sql = "
+            update receipt 
+            set
+            doc_status = 'ออกใบวางบิลแล้ว',
+            updated_date = CURRENT_TIMESTAMP(),
+            updated_by = :action_user
+            where recode = :recode";
+
+            $stmt1 = $conn->prepare($sql);
+            if (!$stmt1) throw new PDOException("Insert data error => {$conn->errorInfo()}");
+
+            $stmt1->bindParam(":action_user", $action_user, PDO::PARAM_INT);
+            $stmt1->bindParam(":recode", $val->recode, PDO::PARAM_STR);
+
+            if (!$stmt1->execute()) {
+                $error = $conn->errorInfo();
+                throw new PDOException("Insert data error => $error");
+                die;
+            }
         }
 
         $sql = "delete from iv_payment where ivcode = :ivcode";
@@ -284,15 +319,14 @@ try {
             if (!$stmt) throw new PDOException("Insert data error => {$conn->errorInfo()}");
 
             $stmt->bindParam(":action_user", $action_user, PDO::PARAM_INT);
-            $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);          
+            $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);
 
             if (!$stmt->execute()) {
                 $error = $conn->errorInfo();
                 throw new PDOException("Insert data error => $error");
                 die;
             }
-        }
-        else if ($total_pay == 0) {
+        } else if ($total_pay == 0) {
             $sql = "
             update iv_master 
             set
@@ -305,15 +339,14 @@ try {
             if (!$stmt) throw new PDOException("Insert data error => {$conn->errorInfo()}");
 
             $stmt->bindParam(":action_user", $action_user, PDO::PARAM_INT);
-            $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);          
+            $stmt->bindParam(":ivcode", $header->ivcode, PDO::PARAM_STR);
 
             if (!$stmt->execute()) {
                 $error = $conn->errorInfo();
                 throw new PDOException("Insert data error => $error");
                 die;
             }
-        }
-        else{
+        } else {
             $sql = "
             update iv_master 
             set
@@ -334,33 +367,56 @@ try {
                 die;
             }
         }
-        
+
         $conn->commit();
         http_response_code(200);
-        echo json_encode(array("data"=> array("code" => $code,"total_pay" => $total_pay,"total_price" => $total_price)));
-    } else  if($_SERVER["REQUEST_METHOD"] == "DELETE"){
+        echo json_encode(array("data" => array("code" => $code, "total_pay" => $total_pay, "total_price" => $total_price)));
+    } else  if ($_SERVER["REQUEST_METHOD"] == "DELETE") {
         // $code = $_DELETE["code"];
         $code = $_GET["code"];
-        
-        $sql = "update receipt set doc_status = 'ยกเลิก' where ivcode = :code";
-        $stmt = $conn->prepare($sql); 
-        if (!$stmt->execute([ 'code' => $code ])){
+
+        $sql = "update iv_master set doc_status = 'ยกเลิก' where ivcode = :code";
+        $stmt = $conn->prepare($sql);
+        if (!$stmt->execute(['code' => $code])) {
             $error = $conn->errorInfo();
             throw new PDOException("Remove data error => $error");
-        }    
+        }
+
+        $strSQL = "SELECT recode FROM iv_detail where ivcode = :code ";
+        $stmt5 = $conn->prepare($strSQL);
+        if (!$stmt5) throw new PDOException("Insert data error => {$conn->errorInfo()}");
+
+
+        if (!$stmt5->execute(['code' => $code])) {
+            $error = $conn->errorInfo();
+            throw new PDOException("Insert data error => $error");
+            die;
+        }
+
+        $res = $stmt5->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($res as $row) {
+
+            $sql = "update receipt set doc_status = 'รอออกใบวางบิล' where recode = :recode";
+            $stmt2 = $conn->prepare($sql);
+            if (!$stmt2->execute(['recode' => $row['recode']])) {
+                $error = $conn->errorInfo();
+                throw new PDOException("Remove data error => $error");
+            }
+        }
 
         $conn->commit();
         http_response_code(200);
-        echo json_encode(array("status"=> 1));
-    } else  if($_SERVER["REQUEST_METHOD"] == "GET"){
-        $code = $_GET["code"]; 
+        echo json_encode(array("status" => 1));
+    } else  if ($_SERVER["REQUEST_METHOD"] == "GET") {
+        $code = $_GET["code"];
         $sql = "SELECT a.*,c.* ";
-        $sql .= " FROM `iv_master` as a left outer join customer as c on (a.cuscode=c.cuscode) ";        
+        $sql .= " FROM `iv_master` as a left outer join customer as c on (a.cuscode=c.cuscode) ";
         $sql .= " where a.ivcode = :code";
 
-        $stmt = $conn->prepare($sql); 
-        if (!$stmt->execute([ 'code' => $code ])){
-            $error = $conn->errorInfo(); 
+        $stmt = $conn->prepare($sql);
+        if (!$stmt->execute(['code' => $code])) {
+            $error = $conn->errorInfo();
             http_response_code(404);
             throw new PDOException("Geting data error => $error");
         }
@@ -392,19 +448,18 @@ try {
 
         $conn->commit();
         http_response_code(200);
-        echo json_encode(array('status' => 1, 'data' => array("header" => $header, "detail" => $detail ,"payment" => $payment)));
+        echo json_encode(array('status' => 1, 'data' => array("header" => $header, "detail" => $detail, "payment" => $payment)));
     }
-
-} catch (PDOException $e) { 
+} catch (PDOException $e) {
     $conn->rollback();
     http_response_code(400);
     echo json_encode(array('status' => '0', 'message' => $e->getMessage()));
-} catch (Exception $e) { 
+} catch (Exception $e) {
     $conn->rollback();
     http_response_code(400);
     echo json_encode(array('status' => '0', 'message' => $e->getMessage()));
-} finally{
+} finally {
     $conn = null;
-}  
-ob_end_flush(); 
+}
+ob_end_flush();
 exit;
