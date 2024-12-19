c012df3c162d9458120f415d0d66cb306d518698
Chayapat <jaroonsoft@hotmail.com>
2024-12-19 12:02:28 +0700

    fix bug api payment

--- api/iv/get-iv_payment.php
+++ api/iv/get-iv_payment.php
@@ -4,15 +4,20 @@ include_once(dirname(__FILE__, 2) . "/onload.php");
 include_once(dirname(__FILE__, 2) . "/common/fnc-code.php");
 $db = new DbConnect;
 $conn = $db->connect();
+$conn->beginTransaction();
+
+
+    $action_date = date("Y-m-d H:i:s");
+    $action_user = $token->userid;
 
 if ($_SERVER["REQUEST_METHOD"] == "GET") {
-    extract($_GET, EXTR_OVERWRITE, "_");
+    $code = $_GET["code"];
 
     $sql = "SELECT p.*,m.*,c.*  ";
     $sql .= " FROM `iv_payment` as p  ";
-    $sql .= " inner join iv_master as m on p.ivcode=m.ivcode  ";
+    $sql .= " left outer join iv_master as m on p.ivcode=m.ivcode  ";
     $sql .= " left outer join customer as c on c.cuscode=m.cuscode  ";
-    $sql .= " where p.ivcode = :code";
+    $sql .= " where p.code = :code";
 
     $stmt = $conn->prepare($sql);
     if (!$stmt->execute(['code' => $code])) {
